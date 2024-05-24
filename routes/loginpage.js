f3c9cba888db527d244720cff39eb9f2752456e5
jeganpetchimuthu <arunjegan1996@gmail.com>
2024-05-24 20:42:38 +0530

    uploadfile

--- routes/loginpage.js
+++ routes/loginpage.js
@@ -21,7 +21,7 @@ const loginSchema = joi.object({
   password: joi.string().min(3).required(),
 });
 
-router.post("/signin", async (req, res) => {
+router.post("/register", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   const user = await Login.findOne({ email });
